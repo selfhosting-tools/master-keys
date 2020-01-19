c6c1ce492d2b16ca2195bbc068b3301ebba3dec3
FL42 <46161216+fl42@users.noreply.github.com>
2020-01-19 11:39:01 +0100

    chore: add LICENCE and minor refactor

--- clone_and_verify.sh
+++ clone_and_verify.sh
@@ -1,17 +1,33 @@
 #!/bin/sh
-# V20200101-1
+
+# Copyright (C) 2020 FL42
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# VERSION: 20200119-1
 
 TRUSTED_GPG_KEYS="CA2B146D7407C0932B96AA8756CD3255CE0673F6"  # space-separated
-REPO="nsd-docker"
-SOURCE_DIRECTORY="$(pwd)/source"
+REPO="nsd-docker"  # example
+CLONE_DIRECTORY="$(pwd)/source"  # example
 
 
-echo "$REPO will be stored in $SOURCE_DIRECTORY..."
+echo "$REPO will be stored in $CLONE_DIRECTORY..."
 docker run \
     -i --rm \
     -e REPO="$REPO" \
     -e TRUSTED_GPG_KEYS="$TRUSTED_GPG_KEYS" \
-    -v "$SOURCE_DIRECTORY":/source \
+    -v "$CLONE_DIRECTORY":/source \
     alpine:latest sh -x -e << \EOF
 # Install deps
 apk add --no-cache git gnupg curl
@@ -21,7 +37,7 @@ cd /source || exit 3
 if [ -d ".git" ]; then
     # Ensure the repository is $REPO (naive verification)
     if ! grep -q "$REPO" .git/config; then
-        echo "$SOURCE_DIRECTORY is not empty and is not $REPO"
+        echo "$CLONE_DIRECTORY is not empty and is not $REPO"
         exit 2
     fi
     git pull
@@ -41,16 +57,16 @@ do
         gpg --import || continue
     [ \
         "$KEY_FP" \
-        == \
+        = \
         "$(git verify-commit HEAD 2>&1 | grep '^Primary key fingerprint:' | \
         cut -d: -f 2 | sed s/' '//g)" \
     ] && VALID_SIG="true" && break; \
 done
-if [ "$VALID_SIG" == "true" ]; then
+if [ "$VALID_SIG" = "true" ]; then
     echo "[OK] Valid signature found"
     exit 0
 else
-    echo "[ERROR] Invalid signature or not signed"
+    echo "[ERROR] Invalid signature, not signed with TRUSTED_GPG_KEYS, or not signed"
     exit 1
 fi
 
